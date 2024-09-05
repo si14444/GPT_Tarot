30c317335c91b13a224985ce5a144aca6adf7479
si14444 <a1@1ui-MacBookAir.local>
2024-09-05 14:16:03 +0900

    프로젝트 초기 세팅

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { React } from "./assets";
+
+function App() {
+  return (
+    <>
+      <div>
+        <React />
+      </div>
+    </>
+  );
+}
+
+export default App;
