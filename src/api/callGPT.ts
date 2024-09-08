61f61d85df71c2296b35c6999f2d2d29641549a5
si14444 <a1@1ui-MacBookAir.local>
2024-09-09 02:04:10 +0900

    feat: gpt 연동

---
+++ src/api/callGPT.ts
@@ -0,0 +1,66 @@
+import useTarot from "../store/useTarot";
+
+interface OpenAIResponse {
+  choices: Array<{
+    finish_reason: string;
+    index: number;
+    logprobs: any; // null 또는 특정 타입을 정의할 수 있습니다
+    message: {
+      content: string;
+    };
+    refusal: any; // null 또는 특정 타입을 정의할 수 있습니다
+    role: string;
+  }>;
+  created: number;
+  id: string;
+  model: string;
+  object: string;
+}
+
+export const callGPT = async (
+  tarot: number[]
+): Promise<OpenAIResponse | null> => {
+  try {
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${import.meta.env.VITE_OPENAIAPI as string}`,
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "user",
+            content: `타로, 연애운, 메이저 아르카나${tarot[0]},메이저 아르카나${tarot[1]},메이저 아르카나${tarot[2]}, 5줄, 카드 하나하나 분석 없이, 종합적으로`,
+          },
+          {
+            role: "assistant",
+            content: `${tarot[0]},${tarot[1]},${tarot[2]}을 종합적으로 해석한 결과는 다음과 같습니다.`,
+          },
+        ],
+        temperature: 0.5,
+        max_tokens: 1000,
+      }),
+    });
+
+    if (!response.ok) {
+      if (response.status === 429) {
+        console.error("Too Many Requests - Rate limit exceeded");
+      } else {
+        // 다른 HTTP 오류 처리
+        const errorData = await response.json();
+        console.error(`Error ${response.status}: ${errorData.error.message}`);
+      }
+      return null; // 오류 발생 시 null 반환
+    }
+
+    const responseData: OpenAIResponse = await response.json();
+    console.log("data", responseData.choices);
+
+    return responseData;
+  } catch (error) {
+    console.error("An unexpected error occurred:", error);
+    return null; // 오류 발생 시 null 반환
+  }
+};
--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -1,8 +1,56 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { BackgrountNoHeader } from "../assets";
+import { tarotImages } from "../assets/constants/tarot";
 import useTarot from "../store/useTarot";
+import { callGPT } from "../api/callGPT";
 
 const ResultPage = () => {
-  const { tarot } = useTarot();
-  return <div>{tarot}</div>;
+  const [result, setResult] = useState<string | null>(null); // 상태를 선언
+  const { tarot, resetTarot } = useTarot();
+  const navigate = useNavigate();
+
+  // API 호출
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await callGPT(tarot);
+      if (data && data.choices.length > 0) {
+        setResult(data.choices[0].message.content);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleReturn = () => {
+    resetTarot();
+    navigate("/");
+  };
+
+  return (
+    <div className="absolute top-0 left-0 flex flex-col justify-center items-center w-screen h-screen">
+      <BackgrountNoHeader className="z-0 absolute top-0 left-0 w-full h-full object-cover" />
+      <div className="z-10 justify-center flex items-start w-screen">
+        <div className="flex gap-20">
+          <img src={tarotImages[tarot[0]]} className="w-full h-full" />
+          <img src={tarotImages[tarot[1]]} className="w-full h-full" />
+          <img src={tarotImages[tarot[2]]} className="w-full h-full" />
+        </div>
+      </div>
+      <div
+        className="z-10 mt-12 p-8 text-lg bg-indigo-800 text-center rounded-3xl"
+        style={{ maxWidth: "90%" }}
+      >
+        {result}
+      </div>
+      <button
+        className="z-10 bg-indigo-500 mt-8 hover:bg-indigo-500"
+        onClick={handleReturn}
+      >
+        돌아가기
+      </button>
+    </div>
+  );
 };
 
 export default ResultPage;
